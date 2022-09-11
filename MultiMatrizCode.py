e8760b7fdd593e33e2362be03548dbf02446dba6
Aleph_W_Alves <alvesuniva@gmail.com>
2022-09-11 15:12:15 -0300

    Update MultiMatrizCode.py

--- MultiMatrizCode.py
+++ MultiMatrizCode.py
@@ -10,7 +10,7 @@ MatrizB = [[5,8,1,2],
           [6,7,3,0],
           [4,5,9,1]]
 
-#matrix Zerarada
+#matriz Zerarada
 result = [[0,0,0,0],
          [0,0,0,0],
          [0,0,0,0]]
@@ -23,14 +23,14 @@ def thread_function(X, Y, result):
     for i in range(len(X)):
         for j in range(len(Y[0])):
             for k in range(len(Y)):
-                result[i][j] += X[i][k] * Y[k][j]
+                result[i][j] += (X[i][k] * Y[k][j])
 
-    print("Resultado de ordenação:\n")
+    print("Resultado da Multiplicação:\n")
     ImprimeMatriz(result)
 
     tempo_fim = time.perf_counter()
-    print(f'\nTask finalizou em {tempo_fim}')
-    print(f'Task durou {tempo_fim - tempo_ini}\n')
+    print(f'\nTask finalizou em: {tempo_fim}')
+    print(f'Task durou:{tempo_fim - tempo_ini}\n')
 
 
 def ImprimeMatriz(M):
@@ -41,13 +41,8 @@ if __name__ == '__main__':
 
     task = th.Thread(target=thread_function, args=(MatrizA,MatrizB,result), name='thread_function')
     task.start()
-    time.sleep(1)
     task.join()
-
-    print("Nome da Thread rodando: {}".format(th.current_thread().name))
+    
     print("ID da task do programa: {}".format(os.getpid()))
     print("Nome da Thread do programa: {}".format(task.name))
     print("ID thread_function:", task.ident)
-   
-
-    
